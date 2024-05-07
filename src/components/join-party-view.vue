f9ed646cad5809fba38e9955bd59d6fcdd7b8e72
Larry R <laurynas.riliskis@gmail.com>
2024-05-06 20:53:52 -0700

    some styling and some changes

--- src/components/join-party-view.vue
+++ src/components/join-party-view.vue
@@ -12,7 +12,6 @@ import { generateClient } from "aws-amplify/api";
 import awsconfig from "@/amplifyconfiguration.json";
 
 Amplify.configure(awsconfig);
-
 const client = generateClient();
 import { createUser } from '@/graphql/mutations';
 
@@ -23,6 +22,10 @@ export default {
       userName: '',
       partyId: ''
     };
+  },
+  created() {
+    // Retrieve partyId from the query parameters if present
+    this.partyId = this.$route.query.partyId || '';
   },
   methods: {
     async joinParty() {
@@ -36,10 +39,9 @@ export default {
         const newUser = await client.graphql({query: createUser, variables: {input: userData}});
         console.log('Created user:', newUser);
         localStorage.setItem('userData', JSON.stringify(newUser.data.createUser));
-        this.$router.push({name: 'Voting', query: {userName:this.userName, partyId: this.partyId}});
+        this.$router.push({name: 'Voting', query: {userName: this.userName, partyId: this.partyId}});
       } catch (error) {
         console.error('Error joining party:', error);
-        // Handle error, if user name exists
       }
     }
   }
@@ -47,8 +49,23 @@ export default {
 </script>
 
 <style scoped>
-.join-party input {
+.join-party {
+  position: fixed;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  width: 90%;
+  max-width: 400px;  /* Limit the maximum width for larger screens */
+  padding: 20px;
+  background-color: #f8f8f8;
+  box-shadow: 0 4px 8px rgba(0,0,0,0.2);  /* Enhanced shadow effect */
+  border-radius: 10px;  /* Optional: Adds rounded corners */
+  text-align: center;  /* Center-aligns the text and button within the inputs */
+}
+
+.join-party input, .join-party button {
   display: block;
-  margin-bottom: 10px;
+  width: 100%;  /* Adjust to full width of the container */
+  margin: 10px 0;
 }
 </style>
